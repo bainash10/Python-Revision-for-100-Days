2633b4b2130c612ff695412d34f76f36cdaf7444
Bainash <nischalbaidar@yahoo.com>
2024-02-24 12:44:18 +0545

    Day 5

--- Day5-Math in Python/1_main.py
+++ Day5-Math in Python/1_main.py
@@ -1,24 +1,32 @@
 friends =10
 
+# Increment
 # friends = friends + 1
 # friends+=1  #Augmented version 
 # print(friends)
 
+# Decrement
 # friends = friends -2
-# print(friends)
 # friends -=2
 # print(friends)
+
+# Multiply
 # friends = friends *3
 # friends*=3
 # print(friends)
+
+# Division
 # friends = friends /2
 # friends /=2
+# print(friends)
 
-
+# exponents
 # friends =friends **2 #2^2
 # friends**=2
+# print(friends)
 
-# remainder = friends%3   #remainder
+#remainder
+# remainder = friends%3   
 friends %=3  #augmented version
 print(friends)
 
