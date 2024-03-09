77873ceef4b6ac709e7b696cbaa5295851d55f6b
Bainash <nischalbaidar@yahoo.com>
2024-03-09 18:20:34 +0545

    Day 19

--- Day19-Countdown Timer/main.py
+++ Day19-Countdown Timer/main.py
@@ -6,8 +6,8 @@ for x in reversed(range(0,my_time)):  #can also be written as:--> for x in range
     seconds=x%60
     minutes= int(x/60)%60
     hours=int(x/3600)
-    print(f"{hours:02}:{minutes:02}:{seconds:02}")
-    time.sleep(1)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}") #The :02 inside each placeholder is a formatting specifier. It means that the variable should be formatted as an integer with at least two digits, and if the value has fewer than two digits, leading zeros should be added.
+    time.sleep(1) #prints another result in 1 seconds delay
     
 
 print("Time's up!")
