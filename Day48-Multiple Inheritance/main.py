dc0213d3ac17f54df4b37d51879f40985709f8ce
Bainash <nischalbaidar@yahoo.com>
2024-04-07 17:23:49 +0545

    Day 48

---
+++ Day48-Multiple Inheritance/main.py
@@ -0,0 +1,33 @@
+ #* multiple inheritance = when a child class is derived from more than one parent class
+
+class Prey:
+    def flee(self):
+        print("This animal flees")
+
+class Predator:
+    def hunt(self):
+        print("This animal is hunting")
+
+class Rabbit(Prey):
+    pass
+
+class Hawk(Predator):
+    pass
+
+class Fish(Prey,Predator):
+    pass
+
+rabbit=Rabbit()
+hawk=Hawk()
+fish=Fish()
+
+
+rabbit.flee()
+hawk.hunt()
+
+print()
+print("As fish can prey and predator small fishes")
+print()
+
+fish.flee()
+fish.hunt()
