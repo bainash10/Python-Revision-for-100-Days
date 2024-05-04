31d97e9f38922c68b80390113fb2df3852c3c9ec
Bainash <nischalbaidar@yahoo.com>
2024-05-04 14:47:59 +0545

    Day 65

---
+++ Day65-Zip Function/main.py
@@ -0,0 +1,12 @@
+ #* zip(*iterables) =  aggregate elements from two or more iterables (list, tuples, sets, etc.)
+ #* creates a zip object with paired elements stored in tuples for each element
+
+usernames = ["Dude", "Bro", "Mister"]
+passwords = ("p@ssword", "abc123", "guest")
+
+users= dict(zip(usernames,passwords))
+
+#print(type(users))     #! prints--> <class 'dict'>
+
+for key,value in users.items():
+    print(key+" : "+value)
